8fe38dd7e7477bfede45c56b46a0b2d8d7008063
Vinicius Takeushi <viniciustakeushi@sisconsultoria.com.br>
2020-10-15 19:06:13 -0300

    axios e blog

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -12,44 +12,12 @@
             </div>
         </div>
         <div class="row" style="margin-bottom:100px; margin-top: 50px">
-          <div class="col-md-4">
+          <div class="col-md-4" v-for="blog in listBlog" :key="blog.blogId">
             <img src="../assets/blog-1.png" alt="blog 1" class="img-responsive" />
-            <p style="color:#0074F8;margin-top:15px">August 2, 2019</p>
-            <p class="colorTitle" style="font-size:18px"><strong>SIS realmente é o melhor lugar para se trabalhar</strong></p>
-            <p class="colorFontDefault">
-              The ongoing machination in Westminster appear to be impacting how we view our personal financial situation...
-            </p>
-            <div class="row">
-              <div class="col-md-6">
-                <span style="color:#EF4025">Leia mais</span>
-              </div>
-              <div class="col-md-6" style="text-align:right">
-                <span><img src="../assets/shareSmall.png" alt="share" /></span>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <img src="../assets/blog-2.png" alt="blog 2" class="img-responsive" />
-            <p style="color:#0074F8;margin-top:15px">August 2, 2019</p>
-            <p class="colorTitle" style="font-size:18px"><strong>Conheça o nosso ambiente de trabalho</strong></p>
-            <p class="colorFontDefault">
-              British consumers have generally remained upbeat since 2016's referendum to leave the EU - partly offsetting weaker
-            </p>
-            <div class="row">
-              <div class="col-md-6">
-                <span style="color:#EF4025">Leia mais</span>
-              </div>
-              <div class="col-md-6" style="text-align:right">
-                <span><img src="../assets/shareSmall.png" alt="share" /></span>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <img src="../assets/blog-3.png" alt="blog 3" class="img-responsive" />
-            <p style="color:#0074F8;margin-top:15px">August 2, 2019</p>
-            <p class="colorTitle" style="font-size:18px"><strong>Central de tecnologia Bradesco e Sis</strong></p>
-            <p class="colorFontDefault">
-              The Gfk survey showed fall across all five measures it use to calculate sentiment, with the steepest decline coming in consumers
+            <p style="color:#0074F8;margin-top:15px">{{blog.dateFull}}</p>
+            <p class="colorTitle" style="font-size:18px"><strong>{{blog.title}}</strong></p>
+            <p class="colorFontDefault" style="overflow-wrap: break-word;">
+              {{blog.description}}
             </p>
             <div class="row">
               <div class="col-md-6">
@@ -65,7 +33,27 @@
 </template>
 
 <script>
+    import axios from 'axios'
+    
     export default {
-        name: 'Blog'
+        name: 'Blog',
+        data: () => {
+          return {
+            listBlog : []
+          }
+        },
+        created() {
+          
+          this.getBlogs();
+        },
+        methods: {
+          getBlogs(){
+            axios.get('api/blog')
+                 .then((result) => 
+                 {
+                   this.listBlog = result.data
+                 });
+          }
+        }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import Blog from './components/Blog.vue'
+import axios from 'axios'
+
+window.axios = axios
+axios.defaults.baseURL = "http://192.168.30.91:5555/"
 
 Vue.config.productionTip = false
 
