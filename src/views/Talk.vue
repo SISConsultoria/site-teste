8a483e3484827b4925c05097cae16b009345585f
Vinicius Takeushi <viniciustakeushi@sisconsultoria.com.br>
2020-10-27 18:21:45 -0300

    employee branding

--- src/views/Talk.vue
+++ src/views/Talk.vue
@@ -1,57 +1,217 @@
 <template>
     <div>
-        <img src="../assets/bannerTalk.png" alt="banner" />
-
+        <img src="../assets/bannerEmployeeBranding.png" alt="banner" style="width:100%" />
         <div class="row colorDefaultSis">
-            <div class="col-md-4">
+            <div class="col-md-6 descriptionWhitePart">
+                <span>
+                    Na SIS buscamos ser uma empresa melhor a cada dia, por 
+                    isso valorizamos pessoas e vitórias. Acreditando na 
+                    importância do momentos de interação, todos os meses 
+                    reservamos um tempo para comemorar os aniversários  do 
+                    período e apresentar aos colaboradores  tudo que 
+                    aconteceu: novos clientes, projetos, agenda de  
+                    treinamentos e campanhas.
+                </span>
             </div>
-             <div class="col-md-4">
-                 <p style="color:white;font-size:30px;padding-top: 8%;padding-bottom: 5%"><strong>Gestão participativa</strong></p>
+             <div class="col-md-6">
             </div>
-             <div class="col-md-4">
-                 <p style="color:white;font-size:30px;padding-top: 8%;"><strong>Palestras e Treinamentos</strong></p>
+        </div>
+        <div class="row colorBackgroundTalk" style="margin-top: 5%">
+            <div class="col-md-6 imgCup">
+            </div>
+            <div class="col-md-6 descriptionCoffeCeoSis">
+                <span>
+                    Acreditamos em uma gestão participativa, desta forma 
+                    realizamos mensalmente o Café com o CEO, um momento 
+                    dedicado a compartilhar ideias e sugestões para a empresa. 
+                    Nossos colaboradores se reúnem com toda a diretoria executiva 
+                    e são ouvidos em uma conversa leve e descontraída. Essa é uma 
+                    forma de valorizar a opinião de todos dentro da organização, o 
+                    que tem gerado bons frutos como diversos projetos.
+                </span>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4">
-            </div>
-             <div class="col-md-4">
-                 <p class="colorDark" style="font-size: 18px;padding-right: 10%;padding-top: 8%;padding-bottom: 10%">
-                     Realizado mensalmente, o Café com o CEO é um
-                     momento de compartilhamento de ideias e sugestões
-                     para a empresa. Nele, nossos colaboradores se 
-                     reúnem com diretoria da SIS e são ouvidos. Uma
-                     forma de valorizar a opinião de todos dentro da
-                     organização que tem gerado bons frutos como os
-                     projetos da Freeco e Copa.
-                 </p>
-            </div>
-             <div class="col-md-4">
-                 <p class="colorDark" style="font-size: 18px;padding-right: 10%;padding-top: 8%">
-                     Promovendo palestras, workshops e eventos internos
-                     ou externos a clientes, com o propósito de oferecer
-                     formação continuada aos nossos profissionais com
-                     as mais atuais tendências de cada área de Tecnologia
-                     da Informação (TI).
-                 </p>
-            </div>
-        </div>
-        <div class="row colorBackgroundTalk">
-            <div class="col-md-6" style="text-align: center">
-                <input type="button" class="btn btn-default" value="CONHEÇA MAIS" style="background-color: #910045;border-color: #910045;color:white;font-weight:bold; width:60%;padding-top:4%;padding-bottom:4%; margin-top:50%" />
+            <div class="col-md-6 imgRunner">
             </div>
             <div class="col-md-6">
-                <img src="../assets/talkIntegraDark.png" alt="banner" style="margin-top: 20%" />
+                <div class="titleGympass">
+                    <span>GYMPASS</span>
+                </div>
+                <div class="gympassExplanation">
+                    O Gympass é um benefício que visa estimular a prática de exercícios e é 
+                    oferecido para todos os colaboradores. Buscamos assim incentivar uma 
+                    melhora na saúde e bem-estar de todos.
+                </div>
+                <div class="gympassTitleHowItWorks">
+                    <span>Como funciona?</span>
+                </div>
+                <div class="gympassExplanationHowItWorks">
+                    <span>
+                        A SIS paga uma mensalidade para que todos tenham acesso a planos mais 
+                        baratos e possam usufruir do benefício em uma grande rede de academias e 
+                        estúdios. Além disso, o Gympass é uma plataforma que conecta 
+                        colaboradores a parceiros que disponibilizam dicas de exercícios, 
+                        meditação, planejamento nutricional e de qualidade de vida, bem como 
+                        aulas online que ficam sempre disponíveis para acesso em qualquer lugar.
+                    </span>
+                </div>
+                <div class="gympassTitlePersonToPerson">
+                    <span>De pessoas para pessoas</span>
+                </div>
+                <div class="gympassExplanationPersonToPerson">
+                    <span>
+                        De pessoas para pessoas! Na SIS nossa preocupação é o bem estar dos nossos colaboradores. 
+                        Acreditamos que investir em uma base sólida, feita por pessoas, nos faz 
+                        ir cada vez mais alto. Por isso nossos funcionários contam com ações 
+                        motivacionais que vão desde momentos de integração a cuidados com 
+                        o corpo e a mente.
+                    </span>
+                </div>
+                <div class="gympassExplanationPersonToPerson gympassExplanationPersonToPersonLast">
+                    <span>Conheça algumas de nossas iniciativas:</span>
+                </div>
+            </div>
+        </div>
+        <div class="row massagePartSis">
+            <div class="col-md-3">
+               <div class="titleDefaultMassage sideTopMassage sideTopFirst sideBetweenTitleMassage">
+                   Massagem
+               </div>
+               <div class="titleDefaultMassage sideTopMassage sideBetweenTitleMassage">
+                   Terapêutica
+               </div>
+               <div class="titleDefaultMassage sideTopMassage sideBottomLast">
+                   Sistêmica
+               </div>
+            </div>
+            <div class="col-md-4 explanationMassageMiddle">
+                <span>
+                     Com ênfase nos canais energéticos da Medicina Chinesa, 
+                     nos pontos de assentimentos energéticos e emocionais da 
+                     coluna. Um trabalho com objetivo de olhar o corpo físico e 
+                     emocional no aspecto sistêmico familiar e das diversas 
+                     formas de se relacionar na vida e com a vida.
+                </span>
+            </div>
+            <div class="col-md-3">
+                <div class="titleTreatment defaultSpaceBottomTreatmentItens">
+                    <span>Para tratar</span>
+                </div>
+                <div class="descriptionTreatmentItens defaultSpaceBottomTreatmentItens">
+                   <span>Estresse</span>
+                </div>
+                 <div  class="descriptionTreatmentItens defaultSpaceBottomTreatmentItens">
+                   <span>Ansiendade</span>
+                </div>
+                <div  class="descriptionTreatmentItens defaultSpaceBottomTreatmentItens">
+                    <span>Pressão alta</span>
+                </div>
+                <div  class="descriptionTreatmentItens defaultSpaceBottomTreatmentItens">
+                   <span>Artrite</span>
+                </div>
+                 <div  class="descriptionTreatmentItens defaultSpaceBottomTreatmentItens">
+                   <span>Baixa imunidade</span>
+                </div>
+                 <div  class="descriptionTreatmentItens defaultSpaceBottomTreatmentItens">
+                   <span>Má circulação</span>
+                </div>
+                <div  class="descriptionTreatmentItens lastDescriptionItenTreatment">
+                    Alinhamento energético
+                </div>
+            </div>
+        </div>
+        <div class="row colorDefaultSis">
+            <div class="col-md-4">
+                <div class="titleFirstColumnMassage">
+                    <span>Quick Massage</span>
+                </div>
+                <div class="descriptionQuickMassage">
+                    Os benefícios da quick massage podem ser 
+                    sentidos imediatamente e só melhoram a cada 
+                    sessão. O alívio de dores musculares, o 
+                    relaxamento das regiões do pescoço, costas e 
+                    braços e a prevenção das lesões por esforço 
+                    repetitivo (LER’s) e dos distúrbios 
+                    osteomusculares relacionados ao trabalho (DORT’s) 
+                    são algumas de suas vantagens.
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="subtitleExplanationQuickMassage">
+                    <span>Além disso, é comprovado que:</span>
+                </div>
+                <div class="itensExplanationQuickMassage itenExplanationFirstQuickMassage">
+                     <span>- Melhora a produtividade;</span> 
+                </div>
+                <div class="itensExplanationQuickMassage">
+                     <span>- Diminui o índice de afastamento por doença;</span> 
+                </div>
+                <div class="itensExplanationQuickMassage">
+                     <span>- Melhora a relação empresa - funcionário;</span>
+                </div>
+                <div class="itensExplanationQuickMassage">
+                     <span>- Auxilia na manutenção da saúde e bem-estar da equipe.</span>
+                </div>
+            </div>
+            <div class="col-md-4 imgMassage">
                 
-                <p style="font-size:30px;color:white;padding-top: 1%;padding-bottom: 2%">
-                    Na SIS, valorizamos pessoas e suas vitórias!
-                    Mensalmente apresentamos aos colaboradores
-                    tudo que ocorreu durante o mês: novos clientes,
-                    projetos, campanhas e ainda celebramos os
-                    aniversariantes do período, uma forma
-                    descontraída de integração e socialização 
-                    todas as áreas.
-                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-7 imgLaboralSis">
+            </div>
+            <div class="col-md-5">
+                <div class="laborGymnasticsTitle">
+                    <span>Ginastisca Laboral</span>
+                </div>
+                <div class="laborGymnasticsSubtitle">
+                    <span>
+                        É realizada algumas vezes por semana, no próprio local de 
+                        trabalho, pelos colaboradores que se interessam. 
+                        A ginástica laboral se dá através da realização coletiva de 
+                        atividades físicas, e dentre seus benefícios estão: 
+                    </span>
+                </div>
+                <div class="laborGymnasticsItem laborGymnasticsFirstItem laborGymnasticsSpaceBetweenItens">
+                    <span>Combate ao sedentarismo;</span>
+                </div>
+                <div class="laborGymnasticsItem laborGymnasticsSpaceBetweenItens">
+                    <span>Melhora no condicionamento físico;</span>
+                </div>
+                <div class="laborGymnasticsItem laborGymnasticsSpaceBetweenItens">
+                    <span>Prevenção de doenças;</span>
+                </div>
+                <div class="laborGymnasticsItem laborGymnasticsSpaceBetweenItens">
+                    <span>Combate ao stress e tensões;</span>
+                </div>
+                <div class="laborGymnasticsItem laborGymnasticsSpaceBetweenItens">
+                    <span>Redução do cansaço mental e físico;</span>
+                </div>
+                <div class="laborGymnasticsItem">
+                    <span>Melhora na autoestima.</span>
+                </div>
+            </div>
+        </div>
+        <div class="row colorDefaultSis">
+            <div class="col-md-12 bigTitleSomosSis">
+                <span>#SomosSis</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 explanationSomosSis">
+                <span>
+                    O ano de 2020 foi marcado com a nossa primeira participação e 
+                    conquista do certificado Great Place to Work (GPTW), um 
+                    reconhecimento dado às empresas que são referência em 
+                    ambiente de trabalho no mundo! 
+                    Nosso sucesso é construído diariamente, em parceria com cada 
+                    colaborador. Através de um time de grandes profissionais que 
+                    atuam com muita dedicação e comprometimento, garantimos a 
+                    entrega dos melhores serviços e soluções aos nossos clientes.
+                </span>
+
+                 <input type="button" class="btn btn-primary buttonWorkWithUs" value="Venha trabalhar conosco" v-on:click="goToWorkWithUs()" />
             </div>
         </div>
         <div class="container" style="margin-top: 2%">
@@ -62,6 +222,16 @@
 
 <script>
     export default {
-        name: 'talk'
+        name: 'talk',
+        methods: {
+            goToWorkWithUs () {
+               window.location.href = "/#/vacancy";
+            }
+        }
     }
 </script>
+
+<style>
+  @import '../assets/css/mainStyles.css';
+  @import '../assets/css/employeeBrandingStyle.css';
+</style>
